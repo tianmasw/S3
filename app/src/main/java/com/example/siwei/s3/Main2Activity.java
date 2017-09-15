871dae76f8da1a57794f9bd051b2650cd3714d1f
司伟 <siwei@siweideMacBook-Air.local>
2017-09-15 13:45:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/siwei/s3/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.example.siwei.s3;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2 );
+
+        TextView e = (TextView)findViewById(R.id.textView);
+
+    }
+}
